ca006ea936172e27111571b7270b26929c225d6e
pragun.pradham <pragun.pradham.contractor@macmillan.com>
2025-06-28 17:03:17 -0500

    host

--- external-app/src/app/api/form-handler/[courseId]/[resourceId]/route.js
+++ external-app/src/app/api/form-handler/[courseId]/[resourceId]/route.js
@@ -4,8 +4,10 @@ export async function POST(request, { params }) {
   const { courseId, resourceId } = await params;
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const redirectUrl = request.nextUrl.clone();
-  redirectUrl.pathname = `/${courseId}/${resourceId}`;
+  //const redirectUrl = request.nextUrl.clone();
+  // redirectUrl.pathname = `/${courseId}/${resourceId}`;
+  const redirectUrl = new URL(`http://${request.headers.get('host')}/${courseId}/${resourceId}`);
+  console.log({ redirectUrl });
 
   Object.entries({ ...data, targetOrigin: request.headers.get('origin') }).forEach(([key, value]) => {
     redirectUrl.searchParams.set(
